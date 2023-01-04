570d1cf5f51b98d09352b0c2a36ffe52e5c7a244
ratisxila <ratiskhirtladze1806@gmail.com>
2023-01-05 01:06:18 +0400

    dumb computer with functions

--- main.cpp
+++ main.cpp
@@ -1,81 +1,94 @@
 #include <iostream>
+#include <cctype>
 #include <vector>
 using namespace std;
 
+int getSmallest(vector<int> &numbers)
+{
+  int smallest{numbers[0]};
+  for (int i{}; i < numbers.size(); i++)
+    if (smallest > numbers.at(i))
+      smallest = numbers.at(i);
+  return smallest;
+}
+
+int getLargest(vector<int> &numbers)
+{
+  int largest{numbers[0]};
+  for (int i{}; i < numbers.size(); i++)
+    if (largest < numbers.at(i))
+      largest = numbers.at(i);
+
+  return largest;
+}
+
+double getMean(vector<int> &numbers)
+{
+  double sum{};
+  for (auto number : numbers)
+    sum += number;
+  return sum / numbers.size();
+}
+
+void addNumber(vector<int> &numbers)
+{
+  int number;
+  cout << "add a number:";
+  cin >> number;
+  numbers.push_back(number);
+}
+
+void printNumbers(vector<int> &numbers)
+{
+  for (int i{}; i < numbers.size(); i++)
+  {
+    cout << numbers[i] << (i != numbers.size() - 1 ? ", " : ".");
+  }
+}
+
+void dispatch(char action, vector<int> &numbers)
+{
+
+  if (tolower(action) != 'a' && numbers.empty())
+  {
+    cout << "no meaning";
+    return;
+  }
+
+  switch (tolower(action))
+  {
+  case 'p':
+    printNumbers(numbers);
+    break;
+  case 'a':
+    addNumber(numbers);
+    break;
+  case 'm':
+    cout << "the mean number is: " << getMean(numbers);
+    break;
+  case 'l':
+    cout << "the largest number is: " << getLargest(numbers);
+    break;
+  case 's':
+    cout << "the smallest number is: " << getSmallest(numbers);
+    break;
+  default:
+    cout<<"please enter a correct letter";
+  }
+}
+
 int main()
 {
-   char mod = '\0';
-   vector<int> cod{};
-   int b{};
+  vector<int> numbers{};
+  int b{};
+  char action = '\0';
+  while (tolower(action) != 'q')
+  {
 
-   while (mod != 'q' && mod != 'Q')
-   {
-
-      cout << "\n\n P-print numbers\n A-add number\n M-display mean for number\n S-display smallest number\n L-display largest number\n Q-quit\n enter:";
-      cin >> mod;
-      if (mod == 'p' || mod == 'P')
-      {
-         if (cod.empty())
-         {
-            cout << "\nno meaning";
-         }
-         else
-         {
-            for (int i{}; i < cod.size(); i++)
-            {
-               cout << cod[i] << (i != cod.size() - 1 ? ", " : ".");
-            }
-         }
-      }
-      else if (mod == 'a' || mod == 'A')
-      {
-         cout << "add number:";
-         cin >> b;
-         cod.push_back(b);
-      }
-      else if (mod == 'm' || mod == 'M')
-      {
-         int all{};
-         for (auto num : cod)
-            all += num;
-         cout << all / cod.size();
-      }
-      else if (mod == 'l' || mod == 'L')
-      {
-         if (cod.empty())
-         {
-            cout << "\nno meaning";
-         }
-         else
-         {
-            int big{cod[0]};
-            for (int i{}; i < cod.size(); i++)
-               if (big < cod.at(i))
-                  big = cod.at(i);
-
-            cout << "the largest number is:" << big;
-         }
-      }
-      else if (mod == 's' || mod == 'S')
-      {
-         if (cod.empty())
-         {
-            cout << "\nno meaning";
-         }
-         else
-         {
-            int small{cod[0]};
-            for (int i{}; i < cod.size(); i++)
-            {
-
-               if (small > cod.at(i))
-               {
-                  small = cod.at(i);
-               }
-            }
-            cout << "the smallest number is:" << small;
-         }
-      }
-   }
-   cout << "Quitting..." << endl;
+    cout << "\tP-print numbers\n\tA-add number\n\tM-display mean for number\n\tS-display smallest number\n\tL-display largest number\n\tQ-quit\n\tenter: ";
+    cin >> action;
+    dispatch(action, numbers);
+    cout << "\n\n";
+  }
+  cout << "Quitting..." << endl;
 }
