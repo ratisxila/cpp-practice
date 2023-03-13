25924b85b68a8af76207785831caa952dc361ba2
ratisxila <ratiskhirtladze1806@gmail.com>
2023-03-13 17:01:45 +0400

    persons list

--- main.cpp
+++ main.cpp
@@ -1,94 +1,24 @@
-#include <iostream>
-#include <cctype>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int getSmallest(vector<int> &numbers)
+struct Person
 {
-  int smallest{numbers[0]};
-  for (int i{}; i < numbers.size(); i++)
-    if (smallest > numbers.at(i))
-      smallest = numbers.at(i);
-  return smallest;
-}
-
-int getLargest(vector<int> &numbers)
-{
-  int largest{numbers[0]};
-  for (int i{}; i < numbers.size(); i++)
-    if (largest < numbers.at(i))
-      largest = numbers.at(i);
-
-  return largest;
-}
-
-double getMean(vector<int> &numbers)
-{
-  double sum{};
-  for (auto number : numbers)
-    sum += number;
-  return sum / numbers.size();
-}
-
-void addNumber(vector<int> &numbers)
-{
-  int number;
-  cout << "add a number:";
-  cin >> number;
-  numbers.push_back(number);
-}
-
-void printNumbers(vector<int> &numbers)
-{
-  for (int i{}; i < numbers.size(); i++)
-  {
-    cout << numbers[i] << (i != numbers.size() - 1 ? ", " : ".");
-  }
-}
-
-void dispatch(char action, vector<int> &numbers)
-{
-
-  if (tolower(action) != 'a' && numbers.empty())
-  {
-    cout << "no meaning";
-    return;
-  }
-
-  switch (tolower(action))
-  {
-  case 'p':
-    printNumbers(numbers);
-    break;
-  case 'a':
-    addNumber(numbers);
-    break;
-  case 'm':
-    cout << "the mean number is: " << getMean(numbers);
-    break;
-  case 'l':
-    cout << "the largest number is: " << getLargest(numbers);
-    break;
-  case 's':
-    cout << "the smallest number is: " << getSmallest(numbers);
-    break;
-  default:
-    cout<<"please enter a correct letter";
-  }
-}
+    string name, lastName;
+    int age = 0;
+};
 
 int main()
 {
-  vector<int> numbers{};
-  int b{};
-  char action = '\0';
-  while (tolower(action) != 'q')
-  {
-
-    cout << "\tP-print numbers\n\tA-add number\n\tM-display mean for number\n\tS-display smallest number\n\tL-display largest number\n\tQ-quit\n\tenter: ";
-    cin >> action;
-    dispatch(action, numbers);
-    cout << "\n\n";
-  }
-  cout << "Quitting..." << endl;
+    int n;
+    cin >> n;
+    vector<Person> persons(n);
+    for (size_t i = 0; i < n; i++)
+    {
+        cout << "Person " << i + 1 << ':' << endl;
+        cin >> persons[i].name >> persons[i].lastName >> persons[i].age;
+    }
+    for (size_t i = 0; i < n; i++)
+    {
+        cout << persons[i].name<<' ' << persons[i].lastName <<' '<< persons[i].age<<endl;
+    }
 }
